114164d6a6b98d7347d32505c52519d19ae7077c
Jon Daniel <binarycleric@gmail.com>
2014-08-16 22:46:53 -0400

    Added title and more randomness

--- main.lua
+++ main.lua
@@ -2,9 +2,12 @@ require "board"
 require "cell"
 require "graphics"
 
+-- http://en.wikipedia.org/wiki/Conway's_Game_of_Life
 function love.load()
   Graphics.cell_size = 5
   last_evolve = 0
+
+  love.window.setTitle("Conway's Game of Life")
 
   love.graphics.setBackgroundColor(25, 25, 25)
   love.window.setMode(800, 650)
@@ -12,7 +15,7 @@ function love.load()
 
   math.randomseed(os.time())
 
-  for i=1, 10000 do
+  for i=1, math.random(10000, 25000) do
     local x = math.random(0, 800/Graphics.cell_size)
     local y = math.random(0, 650/Graphics.cell_size) 
 
